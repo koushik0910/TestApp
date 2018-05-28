3f17da38f5254e8596497ab35ef74f0fa226ad26
Koushik Dutta <koushik0910@gmail.com>
2018-05-28 14:45:09 +0530

    files added

---
+++ TestApp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TestApp
+//
+//  Created by 01HW934413 on 28/05/18.
+//  Copyright © 2018 Koushik Dutta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    func test(){
+        print("Just For Testing")
+    }
+
+}
+
